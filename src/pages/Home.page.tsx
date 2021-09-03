2d5f2d4210c3e66034917c8dc33e3b514703b0e6
Shahram Hosseinabadi <55068890+vocus-shahram@users.noreply.github.com>
2021-09-04 09:14:27 +1000

    use dynamic data

--- src/pages/Home.page.tsx
+++ src/pages/Home.page.tsx
@@ -1,51 +1,33 @@
-import React from "react";
+import { useEffect, useState } from "react";
 import EnhancedTable from "../components/material/table/table.component";
 import { MaterialTable } from "../models";
+import axios from "../api/axios";
+import CircularIndeterminate from "../components/material/spinner/spinner.component";
 const headCells: MaterialTable.HeadCells[] = [
 	{ id: "id", numeric: false, disablePadding: true, label: "ID" },
 	{ id: "email", numeric: false, disablePadding: false, label: "EMAIL" },
 	{ id: "first_name", numeric: false, disablePadding: false, label: "FIRSTNAME" },
 	{ id: "last_name", numeric: false, disablePadding: false, label: "LASTNAME" }
 ];
-
-const rowFromProps = [
-	{
-		id: 7,
-		email: "michael.lawson@reqres.in",
-		first_name: "Michael",
-		last_name: "Lawson"
-	},
-	{
-		id: 8,
-		email: "lindsay.ferguson@reqres.in",
-		first_name: "Lindsay",
-		last_name: "Ferguson"
-	},
-	{
-		id: 9,
-		email: "tobias.funke@reqres.in",
-		first_name: "Tobias",
-		last_name: "Funke"
-	},
-	{
-		id: 10,
-		email: "byron.fields@reqres.in",
-		first_name: "Byron",
-		last_name: "Fields"
-	},
-	{
-		id: 11,
-		email: "george.edwards@reqres.in",
-		first_name: "George",
-		last_name: "Edwards"
-	},
-	{
-		id: 12,
-		email: "rachel.howell@reqres.in",
-		first_name: "Rachel",
-		last_name: "Howell"
-	}
-];
-const HomePage = () => <EnhancedTable headCells={headCells} rowFromProps={rowFromProps} />;
+const HomePage = () => {
+	const [user, setUser] = useState(null);
+	useEffect(() => {
+		axios
+			.get("/api/users?page=2")
+			.then((axiosResponse) => {
+				const {
+					data: { data }
+				} = axiosResponse;
+				console.log("response", data);
+				const mappedUserData = data.map((userData: any) => {
+					const { avatar, ...rest } = userData;
+					return rest;
+				});
+				setUser(mappedUserData);
+			})
+			.catch((error) => console.error("API error", error));
+	}, []);
+	return user ? <EnhancedTable headCells={headCells} rowFromProps={user} /> : <CircularIndeterminate />;
+};
 
 export default HomePage;
