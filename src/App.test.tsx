930355231014aff11f8035704e5e8ec580fe5ac9
Shahram Hosseinabadi <55068890+vocus-shahram@users.noreply.github.com>
2021-09-05 11:35:56 +1000

    testing play around

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,9 +1,17 @@
-import React from 'react';
-import { render, screen } from '@testing-library/react';
-import App from './App';
+import React from "react";
+import { render, screen, cleanup } from "@testing-library/react";
+import renderer from "react-test-renderer";
+import App from "./App";
+afterEach(cleanup);
+test("renders with nab test code in the dom", () => {
+	const headerText = "This is the NAB code challenge.";
+	const { getByTestId } = render(<App />);
+	const linkElement = screen.getByText(headerText);
+	expect(linkElement).toBeInTheDocument();
+	expect(getByTestId("ut-app-header")).toHaveTextContent(headerText);
+});
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+test("snapshot", () => {
+	const tree = renderer.create(<App />);
+	expect(tree).toMatchSnapshot();
 });
--- src/components/header/header.component.tsx
+++ src/components/header/header.component.tsx
@@ -1,5 +1,9 @@
 import React from "react";
 import "./header.style.scss";
-const Header = () => <header aria-label="This page represent the NAB code challenge">This is the NAB code challenge.</header>;
+const Header = () => (
+	<header aria-label="This page represent the NAB code challenge" data-testid="ut-app-header">
+		This is the NAB code challenge.
+	</header>
+);
 
 export default Header;
